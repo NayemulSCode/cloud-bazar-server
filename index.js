ec75f49728974f848b6faf61b2e66d5885879ee9
Nayemul <nayemulhasan2016@gmail.com>
2021-03-30 23:56:25 +0600

    db setup

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const cors = require('cors')
+const MongoClient = require('mongodb').MongoClient;
 require('dotenv').config()
 
 
@@ -14,6 +15,18 @@ const port = process.env.PORT || 5000;
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_UPASS}@cluster0.zqmy8.mongodb.net/${process.env.DB_name}?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+client.connect(err => {
+  const productCollection = client.db("cloudBazar").collection("products");
+  // perform actions on the collection object
+  console.log('db connected!!');
+});
+
+
 
 app.listen(port, () => {
   console.log(`app listening at http://localhost:${port}`)
